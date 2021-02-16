64eb10a014207970a19134292c57fa88b4207ea7
123Aang <20cse0173@vvce.ac.in>
2021-02-16 11:45:03 +0000

    assumed the value of l1 and l2

--- Program_4.c
+++ Program_4.c
@@ -3,11 +3,15 @@
 //L1 = 10
 //L2 = 20
 
-#include<stdio.h>;
+#include<stdio.h>
 int main()
 {
 //Write your code below this line
+int L1=10 , L2=20;
 
+
+printf("L1 = %d\n",L1);
+printf("L2 = %d",L2);
 
 
 return 0;
